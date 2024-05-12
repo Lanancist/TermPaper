cb54adc9ddb440e880081111d4bc977774cbb3f0
Lanancist <Misha_Ushakov04@mail.ru>
2024-05-12 12:08:55 +0300

    Обработка запроса на список анкет (Back)

--- BackEnd/PythonServer/main.py
+++ BackEnd/PythonServer/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def get_hello():
     return "abo baba!"
+
+@app.get("/listAnc")
+def get_list_anc():
+    return {"q": "aboba?", "ansCount": 3, "ans1":"abo", "ans2":"aboba", "ans3":"abobaba"}
